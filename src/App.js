74789c30ea1ac7b9102ff35462279d0b47a031c6
Rafia Khandaker <rafiakhandaker@gmail.com>
2023-05-03 00:11:54 -0400

    Setup request access token

--- src/App.js
+++ src/App.js
@@ -2,22 +2,18 @@ import logo from './logo.svg';
 import './App.css';
 
 function App() {
+  const CLIENT_ID = "ab747e71bc2248c7bfe705d183236c54"
+  const REDIRECT_URI = "http://localhost:3000/auth/callback"
+  const AUTH_ENDPOINT = "https://accounts.spotify.com/authorize"
+  const RESPONSE_TYPE = "token"
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      {/* <header className="App-header">
+      </header> */}
+
+      <a href={`${AUTH_ENDPOINT}?client_id=${CLIENT_ID}&redirect_uri=${REDIRECT_URI}&response_type=${RESPONSE_TYPE}`}>Login to Spotify</a>
+
     </div>
   );
 }
