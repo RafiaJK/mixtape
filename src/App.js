936beff3e7f3ace33d6252c1d3decf05950ae814
Rafia Khandaker <rafiakhandaker@gmail.com>
2023-05-15 14:34:03 -0400

    updated logout

--- src/App.js
+++ src/App.js
@@ -1,5 +1,7 @@
 import logo from './logo.svg';
 import './App.css';
+import { useEffect, useState } from 'react';
+
 
 function App() {
   const CLIENT_ID = "ab747e71bc2248c7bfe705d183236c54"
@@ -7,13 +9,38 @@ function App() {
   const AUTH_ENDPOINT = "https://accounts.spotify.com/authorize"
   const RESPONSE_TYPE = "token"
 
+  const [token, setToken] = useState("")
+
+  useEffect(() => {
+    const hash = window.location.hash
+    let token = window.localStorage.getItem("token")
+
+    if (!token && hash) {
+      token = hash.substring(1).split("&").find(elem => elem.startsWith("access_token")).split("=")[1]
+
+      window.location.hash = ""
+      window.localStorage.setItem("token", token)
+    }
+
+    setToken(token)
+
+  }, [])
+
+
+  const logout = () => {
+    setToken("")
+    window.localStorage.removeItem("token")
+  }
+
   return (
     <div className="App">
       {/* <header className="App-header">
       </header> */}
 
-      <a href={`${AUTH_ENDPOINT}?client_id=${CLIENT_ID}&redirect_uri=${REDIRECT_URI}&response_type=${RESPONSE_TYPE}`}>Login to Spotify</a>
-
+      {!token ?
+        <a href={`${AUTH_ENDPOINT}?client_id=${CLIENT_ID}&redirect_uri=${REDIRECT_URI}&response_type=${RESPONSE_TYPE}`}>Login
+          to Spotify</a>
+        : <button onClick={logout}>Logout</button>}
     </div>
   );
 }
